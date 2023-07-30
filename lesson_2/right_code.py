c156cfcc1dfadd510cf5c82ec203b27f52580ea0
Alex <alex20.babich20@gmail.com>
2023-07-30 11:36:59 +0300

    new code

--- lesson_2/right_code.py
+++ lesson_2/right_code.py
@@ -1,6 +1,6 @@
 smile_footprint = '\U0001F463'
 
-User_name = 'Max'
-User_age = '15'
-Result = f'My name is {User_name}, I am {User_age} years old{smile_footprint}'
-print(Result)
+user_name = 'Max'
+user_age = '15'
+result = f'My name is {user_name}, I am {user_age} years old{smile_footprint}'
+print(result)
