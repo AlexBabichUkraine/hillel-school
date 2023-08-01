1a5cc10e7d7cbdf49469675d49289a931b178131
Alex <alex20.babich20@gmail.com>
2023-08-01 09:34:55 +0300

    01.08 chadged f string

--- lesson_3/What_food.py
+++ lesson_3/What_food.py
@@ -1,7 +1,7 @@
 from pywebio.input import input as pw_input
 from pywebio.output import put_success
 
-favourite_food = pw_input('Enter your favourite food >>>').title()
+favourite_food = pw_input('Enter your favourite food >>>')
 favourite_food = favourite_food.lower()
 emoji = '\U0001F604'
 result = f'oh this food: {favourite_food}, its my  favourite too {emoji}'
