0f26722b81d2ad99933f1889a45c2fff30775f77
Alex <alex20.babich20@gmail.com>
2023-08-24 14:09:50 +0300

    Test function / lesson 11

---
+++ lesson_11/main.py
@@ -0,0 +1,10 @@
+import our_library
+
+
+def main():
+    result = our_library.check_age_and_response(0)
+    print(f"Result of execution function: '{result}'")
+
+
+if __name__ == '__main__':
+    main()
