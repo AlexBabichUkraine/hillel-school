72bfe9b607b66ac8863a229ff1c5a3bcb1a6fc1c
Alex <alex20.babich20@gmail.com>
2023-08-29 12:15:42 +0300

    Car_test

--- lesson_12/car_function_library.py
+++ lesson_12/car_function_library.py
@@ -1,13 +1,20 @@
-def car_parameters(**kwargs):
-    for key in kwargs:
-        if kwargs[key] < 0:
-            raise ValueError('Please enter positive value')
-    distance = kwargs['time'] * kwargs['speed']
-    return f"Car weight of {kwargs['weight']}kg" \
-           f" was moving {kwargs['time']} seconds " \
-           f"with speed {kwargs['speed']}m/s" \
+def car_parameters(*, time: int, speed: int, weight: int) -> str:
+
+    values = {
+        'time': time,
+        'speed': speed,
+        'weight': weight
+    }
+
+    for key, value in values.items():
+        if value < 0:
+            raise ValueError(f"Please enter positive value ({key}: {value})")
+    distance = time * speed
+    return f"Car weight of {weight}kg" \
+           f" was moving {time} seconds " \
+           f"with speed {speed}m/s" \
            f" and traveled {distance} meters"
 
 
 if __name__ == '__main__':
-    print(car_parameters(time=5, speed=2, weight=1200))
+    print(car_parameters(time=7, speed=10, weight=1254))
